1350c124cff112ee438fb69743679ca0d7a5a9c3
mrusli <rusli1621@gmail.com>
2025-09-18 14:08:58 +0700

    update companyDao

--- inventory-mgt/src/main/java/com/pyramix/persistence/company/dao/CompanyDao.java
+++ inventory-mgt/src/main/java/com/pyramix/persistence/company/dao/CompanyDao.java
@@ -1,9 +1,13 @@
 package com.pyramix.persistence.company.dao;
 
+import java.util.List;
+
 import com.pyramix.domain.entity.Ent_Company;
 
 public interface CompanyDao {
 
 	public Ent_Company findCompanyById(long id) throws Exception;
+	
+	public List<Ent_Company> findAllCompany() throws Exception;
 	
 }
